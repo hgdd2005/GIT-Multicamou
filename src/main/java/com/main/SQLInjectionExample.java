91ad7a1e42d0f7130b8fea538124ed84b58b0ae9
hgdd2005 <hgdd2005@gmail.com>
2023-05-31 13:41:53 +0900

    commit2

--- src/main/java/com/main/SQLInjectionExample.java
+++ src/main/java/com/main/SQLInjectionExample.java
@@ -3,7 +3,7 @@ package com.main;
 public class SQLInjectionExample {
     public static void main(String[] args) {
         String username = getUserInput();
-        String query = "SELECT * FROM users WHER E username = '" + username + "'";
+        String query = "SELECT * FROM users WHER E userna me = '" + username + "'";
         // query 실행 로직
     }
 
